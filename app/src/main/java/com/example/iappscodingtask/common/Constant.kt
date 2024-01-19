3f1163809671e2ca8a8253f753b3ec97e5a32ca5
tatvarajesh <rajesh.sathvara@tatvasoft.com>
2024-01-19 11:33:41 +0530

    prepared UI and api response class

---
+++ app/src/main/java/com/example/iappscodingtask/common/Constant.kt
@@ -0,0 +1,18 @@
+package com.example.iappscodingtask.common
+
+/**
+ * Api Base url
+ */
+const val BASE_URL = "https://api.flickr.com"
+
+/**
+ * Api end points
+ */
+const val api_get_photos = "/services/feeds/photos_public.gne"
+
+/**
+ * Api params
+ */
+const val param_format = "format"
+const val param_tags = "tags"
+const val param_nojsoncallback = "nojsoncallback"
